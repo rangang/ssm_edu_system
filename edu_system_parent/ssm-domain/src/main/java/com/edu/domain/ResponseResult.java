776948f5f48351e2aebf9d6a94755d7509d6f9bf
rangang <13452334233@163.com>
2022-06-23 14:03:02 +0800

    课程实体类创建

---
+++ edu_system_parent/ssm-domain/src/main/java/com/edu/domain/ResponseResult.java
@@ -0,0 +1,56 @@
+package com.edu.domain;
+
+/**
+ * @BelongsProject: edu_system_parent
+ * @Author: RG
+ * @CreateTime: 2022/6/23 1:58 下午
+ * @Description: 响应结果封装对象
+ */
+public class ResponseResult {
+    private Boolean success;
+    private Integer state;
+    private String message;
+    private Object content;
+
+    public ResponseResult() {
+    }
+
+    public ResponseResult(Boolean success, Integer state, String message, Object content) {
+        this.success = success;
+        this.state = state;
+        this.message = message;
+        this.content = content;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+}
